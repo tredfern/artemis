2cf9dc03c3e2a8f66c73f58e913ff87a5d778f12
Trevor Redfern <trevorredfern@gmail.com>
2018-08-30 20:16:52 -0700

    My basic implementation of unionfind in lua

---
+++ src/unionfind.lua
@@ -0,0 +1,49 @@
+local UnionFind = {}
+
+function UnionFind:new()
+  local uf = {
+    tree_size = {}
+  }
+
+  setmetatable(uf, self)
+  self.__index = self
+  return uf
+end
+
+function UnionFind:union(x, y)
+  local rootX = self:find(x)
+  local rootY = self:find(y)
+  self:merge_trees(rootX, rootY)
+end
+
+function UnionFind:merge_trees(x, y)
+  -- Use weighting based on the size of the tree
+  if self.tree_size[x] > self.tree_size[y] then
+    self[y] = x
+    self.tree_size[x] = self.tree_size[x] + self.tree_size[y]
+  else
+    self[x] = y
+    self.tree_size[y] = self.tree_size[y] + self.tree_size[x]
+  end
+end
+
+function UnionFind:find(x)
+  -- If it doesn't exist let's add it
+  if self[x] == nil then
+    self[x] = x
+    self.tree_size[x] = 1
+  end
+
+  if self[x] == x then
+    return x
+  else
+    return self:find(self[x])
+  end
+end
+
+function UnionFind:connected(ref1, ref2)
+  -- find if they have the same root
+  return self:find(ref1) == self:find(ref2)
+end
+
+return UnionFind
