f12a45b81be96960d95e3ad5547a9f7a6194f40d
Trevor Redfern <trevorredfern@gmail.com>
2018-10-07 13:29:11 -0700

    Implemented Mergesort

--- src/shellsort.lua
+++ src/shellsort.lua
@@ -3,11 +3,7 @@
 -- This software is released under the MIT License.
 -- https://opensource.org/licenses/MIT
 
-local defaultCompare = function(a, b)
-  if(b == nil) then return true end
-  if(a == nil) then return false end
-  return a <= b
-end
+local defaultCompare = require "src.defaultsortcompare"
 
 local ShellSort = function(set, compare)
   compare = compare or defaultCompare
