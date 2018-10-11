8ca6b3d8fd18e2ff1f00599d8ba5abf02a8a7178
Trevor Redfern <trevorredfern@gmail.com>
2018-10-11 06:14:06 -0700

    Moved out of src directory to make library cleaner

--- src/README.md
+++
@@ -1,1 +0,0 @@
-Source files go into this directory
--- test_helpers/mock_random.lua
+++ test_helpers/mock_random.lua
@@ -3,7 +3,7 @@
 -- This software is released under the MIT License.
 -- https://opensource.org/licenses/MIT
 
-local Queue = require "src.queue"
+local Queue = require "queue"
 local results = Queue:new()
 local oldrandom = math.random
 
